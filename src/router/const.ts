16b600d634b2bce28814179cd6c77fdbad61ce80
alyonium <alyonium2.0@gmail.com>
2025-07-07 21:39:14 +0200

    deploy settings

--- src/router/const.ts
+++ src/router/const.ts
@@ -1,11 +1,13 @@
+export const BASE_PATH = '/rick-and-morty';
+
 export const CARD_MODE = {
-  EDIT: "edit",
-  VIEW: "view",
+  EDIT: 'edit',
+  VIEW: 'view',
 };
 
 export const ROUTE = {
-  CATALOG: "/rick-and-morty/catalog",
-  CARD: "/rick-and-morty/card",
-  CARD_EDIT: `/card/${CARD_MODE.EDIT}`,
-  CARD_VIEW: `/card/${CARD_MODE.VIEW}`,
+  CATALOG: `${BASE_PATH}/catalog`,
+  CARD: `${BASE_PATH}/card`,
+  CARD_EDIT: `${BASE_PATH}/card/${CARD_MODE.EDIT}`,
+  CARD_VIEW: `${BASE_PATH}/card/${CARD_MODE.VIEW}`,
 };
