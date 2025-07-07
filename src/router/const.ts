6e19bf7c8249ccb7b90ed2a08dea1e8be495e656
alyonium <alyonium2.0@gmail.com>
2025-07-07 17:50:11 +0200

    update router paths

--- src/router/const.ts
+++ src/router/const.ts
@@ -1,11 +1,11 @@
 export const CARD_MODE = {
-  EDIT: 'edit',
-  VIEW: 'view',
+  EDIT: "edit",
+  VIEW: "view",
 };
 
 export const ROUTE = {
-  CATALOG: '/catalog',
-  CARD: '/card',
+  CATALOG: "/",
+  CARD: "/card",
   CARD_EDIT: `/card/${CARD_MODE.EDIT}`,
   CARD_VIEW: `/card/${CARD_MODE.VIEW}`,
 };
